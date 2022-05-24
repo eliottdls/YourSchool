6753a026fc95b64ec6217b68b53c39c38b56879d
camilleperrin26 <camilleperrin26@gmail.com>
2022-05-24 14:40:27 +0200

    d

--- School.java
+++ School.java
@@ -8,49 +8,31 @@ public class School {
      */
 	private String nom;
 
-    /** Attribut d'un joueur à visibilité privée.
-     * Strategie du joueur
-     */
-	private Strategie strategie;
+  private List<String> preference;
+
 
 	/** Construire un joueur à partir de son nom et de sa strategie.
 	 * @param nom nom du joueur
 	 * @param strategie strategie du joueur
 	 */
-	public Joueur(String nom, Strategie strategie) {
+	public School(String nom; List<String> preference) {
 		assert nom != null;
 		this.nom = nom;
-		this.strategie = strategie;
 	}
 
 	/** Obtenir le nom d'un joueur.
 	 * @return le nom du joueur
 	 */
 	public String getNom() {
-		String j = this.nom;
-		return j;
-	}
-
-	/** Obtenir la stratégie du joueur.
-	 * @return la stratégie du joueur.
-	 */
-	public Strategie getStrategie() {
-		Strategie s = strategie;
-		return s;
-	}
-
-	/** Obtenir la prise au prochain coup d'un joueur.
-	 * @return la prise au prochain coup d'un joueur
-	 */
-	public int getPrise(Jeu jeu) {
-		return strategie.getPrise(jeu);
+		String n = this.nom;
+		return n;
 	}
 
     /** Afficher le jeu.  Le jeu est affiché sous la forme :
      *
 	 */
     public String toString() {
-    	return "Joueur " + this.nom;
+    	return "Ecole " + this.nom;
     }
 
 	/** Changer le nom du joueur.
